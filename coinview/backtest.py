34c8faf0d7e0c7939660a4ff22e712c1672c026a
Mikelew88 <mikelew88@gmail.com>
2021-04-23 12:24:03 -0600

    add inital files with bot experiments

--- coinview/backtest.py
+++ coinview/backtest.py
@@ -1,30 +1,112 @@
 import backtrader as bt
 import datetime
 
-class RSIStrategy(bt.Strategy):
+start_time = "2021-01-19"
+end_time = "2021-04-23"
 
-    def __init__(self):
-        self.rsi = bt.talib.RSI(self.data, period=14)
+MAX_POSITIONS = 1000
+POSITION_DECAY = 0
+BUY_SELL_DESIRE_DECAY = 0
+RSI_OVERBOUGHT = 70
+RSI_OVERSOLD = 30
+PERIOD = 8
+
+
+class RSIStrategy(bt.Strategy):
+    def __init__(
+        self, rsi_overbought=RSI_OVERBOUGHT, rsi_oversold=RSI_OVERSOLD, period=PERIOD
+    ):
+        # 4*10
+        self.rsi = bt.talib.RSI(self.data, timeperiod=period)
+        self.periods_without_buy = 0
+        self.periods_without_sell = 0
+        self.rsi_overbought = rsi_overbought
+        self.rsi_oversold = rsi_oversold
+        self.period = period
 
     def next(self):
-        if self.rsi < 30 and not self.position:
+        self.periods_without_buy += 1
+        self.periods_without_sell += 1
+        last_rsi = self.rsi[-1]
+        if (
+            last_rsi
+            < (
+                self.rsi_oversold
+                + (self.periods_without_buy * BUY_SELL_DESIRE_DECAY)
+                - (self.position.size * POSITION_DECAY)
+            )
+            and self.position.size <= MAX_POSITIONS
+        ):
             self.buy(size=1)
-        
-        if self.rsi > 70 and self.position:
-            self.close()
+            self.periods_without_buy = 0
 
+        if (
+            last_rsi
+            > (
+                self.rsi_overbought
+                - (self.periods_without_sell * BUY_SELL_DESIRE_DECAY)
+                - (self.position.size * POSITION_DECAY)
+            )
+            and self.position.size > 0
+        ):
+            self.close()
+            self.periods_without_sell = 0
+
+    def stop(self):
+        print(
+            "(Period %2d) (RSI_OVER_BOUGHT %2d) (RSI_OVER_SOLD %2d) Ending Value %.2f"
+            % (
+                self.period,
+                self.rsi_overbought,
+                self.rsi_oversold,
+                self.broker.getvalue(),
+            )
+        )
+
+
+import config, csv
+from binance.client import Client
+
+client = Client(config.API_KEY, config.API_SECRET)
+
+csvfile = open("coinview/data/backtest.csv", "w", newline="")
+candlestick_writer = csv.writer(csvfile, delimiter=",")
+
+candlesticks = client.get_historical_klines(
+    "ETHUSDT", Client.KLINE_INTERVAL_15MINUTE, start_time, end_time
+)
+
+for candlestick in candlesticks:
+    candlestick[0] = candlestick[0] / 1000
+    candlestick_writer.writerow(candlestick)
+
+csvfile.close()
 
 cerebro = bt.Cerebro()
 
-fromdate = datetime.datetime.strptime('2020-07-01', '%Y-%m-%d')
-todate = datetime.datetime.strptime('2020-07-12', '%Y-%m-%d')
+fromdate = datetime.datetime.strptime(start_time, "%Y-%m-%d")
+todate = datetime.datetime.strptime(end_time, "%Y-%m-%d")
 
-data = bt.feeds.GenericCSVData(dataname='data/2020_15minutes.csv', dtformat=2, compression=15, timeframe=bt.TimeFrame.Minutes, fromdate=fromdate, todate=todate)
+data = bt.feeds.GenericCSVData(
+    dataname="coinview/data/backtest.csv",
+    dtformat=2,
+    compression=15,
+    timeframe=bt.TimeFrame.Minutes,
+    fromdate=fromdate,
+    todate=todate,
+)
 
-cerebro.adddata(data)
+# cerebro.optstrategy(
+#     RSIStrategy,
+#     # rsi_overbought=range(70, 100, 5),
+#     # rsi_oversold=range(10, 35, 5),
+#     period=range(4, 4*60, 4)
+# )
 
 cerebro.addstrategy(RSIStrategy)
 
-cerebro.run()
+cerebro.adddata(data)
+
+cerebro.run(maxcpus=1)
 
 cerebro.plot()
--- rsibot/bot.py
+++ rsibot/bot.py
@@ -5,21 +5,29 @@ from binance.enums import *
 
 SOCKET = "wss://stream.binance.com:9443/ws/ethusdt@kline_1m"
 
-RSI_PERIOD = 14
+RSI_PERIOD = 8
 RSI_OVERBOUGHT = 70
 RSI_OVERSOLD = 30
-TRADE_SYMBOL = 'ETHUSD'
-TRADE_QUANTITY = 0.05
+TRADE_SYMBOL = "ETHUSD"
+TRADE_QUANTITY = 0.02
+ORDER_TYPE_MARKET = "MARKET"
+SIDE_BUY = "BUY"
+SIDE_SELL = "SELL"
+MAX_POSTITIONS = 5
+POSITION_DECAY = 0
 
 closes = []
-in_position = False
+n_positions = 0
 
-client = Client(config.API_KEY, config.API_SECRET, tld='us')
+client = Client(config.API_KEY, config.API_SECRET, tld="us")
 
-def order(side, quantity, symbol,order_type=ORDER_TYPE_MARKET):
+
+def order(side, quantity, symbol, order_type=ORDER_TYPE_MARKET):
     try:
         print("sending order")
-        order = client.create_order(symbol=symbol, side=side, type=order_type, quantity=quantity)
+        order = client.create_order(
+            symbol=symbol, side=side, type=order_type, quantity=quantity
+        )
         print(order)
     except Exception as e:
         print("an exception occured - {}".format(e))
@@ -27,59 +35,58 @@ def order(side, quantity, symbol,order_type=ORDER_TYPE_MARKET):
 
     return True
 
-    
+
 def on_open(ws):
-    print('opened connection')
+    print("opened connection")
+
 
 def on_close(ws):
-    print('closed connection')
+    print("closed connection")
+
 
 def on_message(ws, message):
-    global closes, in_position
-    
-    print('received message')
+    global closes, n_positions
+
+    print("received message")
     json_message = json.loads(message)
     pprint.pprint(json_message)
 
-    candle = json_message['k']
+    candle = json_message["k"]
 
-    is_candle_closed = candle['x']
-    close = candle['c']
+    is_candle_closed = candle["x"]
+    close = candle["c"]
 
     if is_candle_closed:
         print("candle closed at {}".format(close))
         closes.append(float(close))
-        print("closes")
-        print(closes)
 
         if len(closes) > RSI_PERIOD:
             np_closes = numpy.array(closes)
             rsi = talib.RSI(np_closes, RSI_PERIOD)
-            print("all rsis calculated so far")
-            print(rsi)
             last_rsi = rsi[-1]
-            print("the current rsi is {}".format(last_rsi))
 
-            if last_rsi > RSI_OVERBOUGHT:
-                if in_position:
+            if last_rsi > RSI_OVERBOUGHT - (n_positions * POSITION_DECAY):
+                if n_positions > 1:
                     print("Overbought! Sell! Sell! Sell!")
                     # put binance sell logic here
                     order_succeeded = order(SIDE_SELL, TRADE_QUANTITY, TRADE_SYMBOL)
                     if order_succeeded:
-                        in_position = False
+                        n_positions -= 1
                 else:
                     print("It is overbought, but we don't own any. Nothing to do.")
-            
-            if last_rsi < RSI_OVERSOLD:
-                if in_position:
-                    print("It is oversold, but you already own it, nothing to do.")
+
+            if last_rsi < RSI_OVERSOLD - (n_positions * POSITION_DECAY):
+                if n_positions <= MAX_POSTITIONS:
+                    print("It is oversold, but you already own max postitions, nothing to do.")
                 else:
                     print("Oversold! Buy! Buy! Buy!")
                     # put binance buy order logic here
                     order_succeeded = order(SIDE_BUY, TRADE_QUANTITY, TRADE_SYMBOL)
                     if order_succeeded:
-                        in_position = True
+                        n_positions += 1
 
-                
-ws = websocket.WebSocketApp(SOCKET, on_open=on_open, on_close=on_close, on_message=on_message)
+
+ws = websocket.WebSocketApp(
+    SOCKET, on_open=on_open, on_close=on_close, on_message=on_message
+)
 ws.run_forever()
